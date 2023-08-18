07c4f75758513e05b8b3260de619b2518d0dd582
George <zhorenty@gmail.com>
2023-08-18 23:11:38 +0300

    Add docs

--- lib/src/core/theme/theme.dart
+++ lib/src/core/theme/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// Light theme of this application.
 final $lightThemeData = ThemeData(
   colorScheme: const ColorScheme.light(
     primary: Color(0xFFFFFFFF),
@@ -8,6 +9,7 @@ final $lightThemeData = ThemeData(
   useMaterial3: true,
 );
 
+/// Dark theme of this application.
 final $darkThemeData = ThemeData(
   colorScheme: const ColorScheme.dark(
     primary: Color(0xFF000000),
